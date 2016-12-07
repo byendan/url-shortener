636fc265558cc8abac21b18c5711f13fca152de3
Brendan <byendanb@gmail.com>
2016-12-07 21:00:53 +0000

    Can get short urls generated, and redirect when using short urls

--- server.js
+++ server.js
@@ -34,15 +34,47 @@ app.get('/', function (req, res) {
 
 // Request with a shortened url will either find it or return error
 app.get('/:short_url', function(req, res) {
-    var inputUrl = req.params.short_url
     
+    MongoClient.connect(dburl, function(err, db) {
+        var inputUrl = req.params.short_url
+        console.log("the input url was: " + inputUrl)
+
+        if(err) throw err 
+        var collection = db.collection('urls')
+        var dbQuery = {"short": parseInt(inputUrl)}
+        console.log(JSON.stringify(dbQuery))
+        collection.findOne(dbQuery, function(err, addressObject) {
+            if (err) throw err 
+            
+            console.log("I got this from the db: " + addressObject.original)
+            var redirectUrl = addressObject.original
+            console.log("Trying to redirect you to " + redirectUrl)
+            db.close()
+            res.redirect(redirectUrl);
+            
+        })
+    })
 })
 
+
 // Someone wants to shorten a new url
-app.post('/new/:url', function(req, res) {
-    var inputUrl = req.params.url
+app.get('/new/*', function(req, res) {
+    var fullUrl = req.originalUrl.split("/")
+    console.log(fullUrl[2] + '//' + fullUrl[4])
+    var inputUrl = fullUrl[2] + '//' + fullUrl[4]
     if(validUrl(inputUrl)) {
-        
+        var shortenedUrl = convertUrl(inputUrl)
+        MongoClient.connect(dburl, function(err, db) {
+            if(err) throw err
+            var collection = db.collection('urls')
+            collection.insertOne({"short": shortenedUrl, "original": inputUrl}, function(err, result) {
+                if(err) throw err 
+                res.send(JSON.stringify({"Original_url": inputUrl, "Shortened_url": shortenedUrl}))
+                db.close()
+            })
+        })
+    } else {
+        res.send("Invalid url was provided")
     }
 })
 
@@ -53,7 +85,7 @@ app.listen((process.env.PORT || 8080), function () {
 function validUrl(testUrl) {
     // first verify hypertext prefix
     var prefix = testUrl.split(":")[0]
-    if(prefix != "http" || prefix != "https") {
+    if(prefix != "http" && prefix != "https") {
         return false
     }
     // check it has a valid ending
@@ -61,17 +93,22 @@ function validUrl(testUrl) {
     suffix = suffix[suffix.length - 1]
     
     // check valid port format
-    if(suffix.split("").includes(":")) {
+    if(suffix.indexOf(":") > -1) {
         if(suffix.split(":").length != 2) {
             return false
         }
         
         // Make sure a port is either 2 or 4 numbers long
-        if(suffix.split(":")[1].length != 2 || suffix.split(":")[1].length != 4) {
+        if(suffix.split(":")[1].length != 2 && suffix.split(":")[1].length != 4) {
             return false
         }
     }
     return true
 }
 
+function convertUrl(original) {
+    var fullHash = require('crypto').createHash('md5').update(original).digest("hex")
+    var hashDec = Math.floor(parseInt(fullHash, 16) / Math.pow(10, 32) )
+    return hashDec
+}
 
